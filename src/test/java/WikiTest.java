c13a4845f967c62b1d1dad1b9698f6d103aaa42b
MaxxPC <maxkanz1991@gmail.com>
2024-09-18 22:56:34 +0400

    added .WikiTest

---
+++ src/test/java/WikiTest.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WikiTest {
+    @BeforeAll
+    public static void beforeAll()
+    {
+        Configuration.baseUrl = "https://github.com"; // открываем страницу Selenide в GitHub (абсолютный путь)
+        Configuration.pageLoadStrategy = "eager"; // команда для того, чтобы селенид не ждал загрузки всех картинок и тяжелых элементов. Только html.
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void WikiTestSelenide() {
+        open("/selenide/selenide"); // открываем страницу Selenide в GitHub (относительный путь)
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $$("ul.filterable-active div a").findBy(text("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("""
+                Using JUnit5 extend test class:
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
+    }
+}
